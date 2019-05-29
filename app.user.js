de8f7c96144c9c51bc3ed37a26356480dbefd65d
woolition <woolition@gmail.com>
2019-05-29 12:11:24 +0800

    main

---
+++ app.user.js
@@ -0,0 +1,37 @@
+// ==UserScript==
+// @name         美团商家工具集
+// @namespace    https://greasyfork.org/zh-CN/users/104201
+// @version      1.1
+// @updateURL    https://raw.githubusercontent.com/woolition/himudy/master/app.user.js
+// @downloadURL  https://raw.githubusercontent.com/woolition/himudy/master/app.user.js
+// @description  美团景酒商家工具，不对外
+// @author       黄盐
+// @match        https://dabao.meituan.com/trippackage/apps.html
+// @include      https://dabao.meituan.com/trippackage/api/v2/deal/*/packages*
+// @run-at       document-end
+// @grant        GM_xmlhttpRequest
+// @grant        GM_addStyle
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_notification
+// @grant        GM_registerMenuCommand
+// @grant        GM_addValueChangeListener
+// @require      https://raw.githubusercontent.com/woolition/himudy/master/vue.min.js
+// @require      https://raw.githubusercontent.com/woolition/himudy/master/fuzzy.min.js
+// @require      https://raw.githubusercontent.com/woolition/himudy/master/Sortable.min.js
+// @require      https://raw.githubusercontent.com/woolition/himudy/master/mtApp.css
+// @require      https://raw.githubusercontent.com/woolition/himudy/master/manageData.md.jpg
+// @require      https://raw.githubusercontent.com/woolition/himudy/master/mtapp.UI.jpg
+// @require      https://raw.githubusercontent.com/woolition/himudy/master/dialog.md.jpg
+// @require      https://raw.githubusercontent.com/woolition/himudy/master/mtGetPriceAndStock.md.jpg
+// @require      https://raw.githubusercontent.com/woolition/himudy/master/makeComparePorject.md.jpg
+// @require      https://raw.githubusercontent.com/woolition/himudy/master/PriceComparer.md.jpg
+// @require      https://raw.githubusercontent.com/woolition/himudy/master/mtDoCompare.jpg
+// @require      https://raw.githubusercontent.com/woolition/himudy/master/productInfo.md.jpg
+// ==/UserScript==
+;/* jshint esversion: 6 */
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
