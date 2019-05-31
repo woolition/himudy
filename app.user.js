1719c74fdc7f66795bb1ad085d5f89d937a4590c
woolition <woolition@gmail.com>
2019-05-31 15:17:07 +0800

    版本V1.3

--- app.user.js
+++ app.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         美团商家工具集
 // @namespace    https://raw.githubusercontent.com/woolition/himudy/master/app.user.js
-// @version      1.2
+// @version      1.3
 // @updateURL    https://raw.githubusercontent.com/woolition/himudy/master/app.user.js
 // @downloadURL  https://raw.githubusercontent.com/woolition/himudy/master/app.user.js
-// @description  美团景酒商家工具，不对外,测试一下更新
+// @description  美团景酒商家工具，不对外
 // @author       ╮(╯▽╰)╭
 // @match        https://dabao.meituan.com/trippackage/apps.html
 // @include      https://dabao.meituan.com/trippackage/api/v2/deal/*/packages*
@@ -23,8 +23,8 @@
 // @require      https://raw.githubusercontent.com/woolition/himudy/master/manageData.md.jpg
 // @require      https://raw.githubusercontent.com/woolition/himudy/master/mtapp.UI.jpg
 // @require      https://raw.githubusercontent.com/woolition/himudy/master/dialog.md.jpg
-// @require      https://raw.githubusercontent.com/woolition/himudy/master/mtGetPriceAndStock.md.jpg
-// @require      https://raw.githubusercontent.com/woolition/himudy/master/makeComparePorject.md.jpg
+// @require      https://raw.githubusercontent.com/woolition/himudy/master/mtGetPriceAndStock.2019-5-31.jpg
+// @require      https://raw.githubusercontent.com/woolition/himudy/master/makeComparePorject.2019-5-31.jpg
 // @require      https://raw.githubusercontent.com/woolition/himudy/master/PriceComparer.md.jpg
 // @require      https://raw.githubusercontent.com/woolition/himudy/master/mtDoCompare.jpg
 // @require      https://raw.githubusercontent.com/woolition/himudy/master/productInfo.md.jpg
