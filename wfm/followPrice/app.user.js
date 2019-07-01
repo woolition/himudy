ebd74a264e62de891b92d7ac8c420279d5805eb0
woolition <woolition@gmail.com>
2019-07-01 14:50:51 +0800

    new toolkit version 1.0

---
+++ wfm/followPrice/app.user.js
@@ -0,0 +1,43 @@
+// ==UserScript==
+// @name         马蜂窝商家工具集
+// @namespace    https://raw.githubusercontent.com/woolition/himudy/master/wfm/followPrice/app.user.js
+// @version      1.0
+// @updateURL    https://raw.githubusercontent.com/woolition/himudy/master/wfm/followPrice/app.user.js
+// @downloadURL  https://raw.githubusercontent.com/woolition/himudy/master/wfm/followPrice/app.user.js
+// @description  马蜂窝景酒商家工具，不对外 更新时间 2019-7-1 14:43:36
+// @author       ╮(╯▽╰)╭
+// @include      *://www.mafengwo.cn/sales/detail/stock/detail
+// @include      *://www.mafengwo.cn/sales/detail/stock/detail?groupId=*
+// @include      *://www.mafengwo.cn/sales/detail/index/sku?id=*
+// @run-at       document-end
+// @grant        GM_xmlhttpRequest
+// @grant        GM_addStyle
+// @grant        GM_getValue
+// @grant        GM_setValue
+// @grant        GM_notification
+// @grant        GM_registerMenuCommand
+// @grant        GM_addValueChangeListener
+// @require      https://raw.githubusercontent.com/woolition/himudy/master/vue.min.js
+// @require      https://raw.githubusercontent.com/woolition/himudy/master/nacl.js
+// @require      https://raw.githubusercontent.com/woolition/himudy/master/Sortable.min.js
+// @require      https://raw.githubusercontent.com/woolition/himudy/master/fuzzy.min.js
+// @require      https://raw.githubusercontent.com/woolition/himudy/master/wfm/followPrice/config.2019-7-1.jpg
+// @require      https://raw.githubusercontent.com/woolition/himudy/master/mtApp.css
+// @require      https://raw.githubusercontent.com/woolition/himudy/master/wfm/followPrice/mtGetPrice.2019-7-1.jpg
+// @require      https://raw.githubusercontent.com/woolition/himudy/master/makeComparePorject.2019-7-1.jpg
+// @require      https://raw.githubusercontent.com/woolition/himudy/master/wfm/followPrice/productInfo.2019-7-1.jpg
+// @require      https://raw.githubusercontent.com/woolition/himudy/master/dragSelect.2019-6-5.jpg
+// @require      https://raw.githubusercontent.com/woolition/himudy/master/syncPNS.2019-7-1.jpg
+// @require      https://raw.githubusercontent.com/woolition/himudy/master/followPrice.2019-7-1.jpg
+// @require      https://raw.githubusercontent.com/woolition/himudy/master/manageData.2019-7-1.jpg
+// @require      https://raw.githubusercontent.com/woolition/himudy/master/mtapp.UI.2019-7-1.jpg
+// @require      https://raw.githubusercontent.com/woolition/himudy/master/dialog.2019-7-1.jpg
+// @require      https://raw.githubusercontent.com/woolition/himudy/master/PriceComparer.2019-7-1.jpg
+// @require      https://raw.githubusercontent.com/woolition/himudy/master/mtDoCompare.2019-7-1.jpg
+// ==/UserScript==
+;/* jshint esversion: 6 */
+(function() {
+    'use strict';
+
+    // Your code here...
+})();
